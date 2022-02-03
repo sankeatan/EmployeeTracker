7b2e3dff7896bf6b27656999c3c0a031f25c700d
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-02-02 23:27:22 -0500

    fixed index.js/connection.js mixup

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,13 @@
-const express = require('express');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const connection = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL username,
       user: 'root',
-      // MySQL password
       password: 'rootroot',
       database: 'organization_db'
     },
     console.log(`Connected to the organization_db database.`)
   );
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Query database
 conection.query('SELECT * FROM students', function (err, results) {
   console.log(results);
